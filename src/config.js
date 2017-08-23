4224eea8edba23f73a12a54051a097532f6d6037
DPLV\dpoz <dpoznyak@gmail.com>
2017-08-23 21:41:50 +0800

    attempt to remove failingleftover code

--- src/config.js
+++ src/config.js
@@ -24,38 +24,15 @@ const config = {
     env: process.env.NODE_ENV || 'development',
     root: path.join(__dirname, '..'),
     port: process.env.PORT || 9000,
-    ip: process.env.IP || '0.0.0.0',
-    masterKey: requireProcessEnv('MASTER_KEY'),
-    mongo: {
-      options: {
-        db: {
-          safe: true
-        }
-      }
-    }
+    ip: process.env.IP || '0.0.0.0'
   },
   test: {
-    mongo: {
-      uri: 'mongodb://localhost/urmr-webrenderer-test',
-      options: {
-        debug: false
-      }
-    }
   },
   development: {
-    mongo: {
-      uri: 'mongodb://localhost/urmr-webrenderer-dev',
-      options: {
-        debug: true
-      }
-    }
   },
   production: {
     ip: process.env.IP || undefined,
-    port: process.env.PORT || 8080,
-    mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/urmr-webrenderer'
-    }
+    port: process.env.PORT || 8080
   }
 }
 
