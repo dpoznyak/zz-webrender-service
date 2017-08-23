756c479c1a13ab385048263641f534e25ba5fea8
DPLV\dpoz <dpoznyak@gmail.com>
2017-08-23 20:56:07 +0800

    moving to standalone repo

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import { Router } from 'express'
+//import {htmlConvert} from 'html-convert'
+
+var htmlConvert = require('html-convert');
+
+var convert = htmlConvert();
+
+const router = new Router()
+
+/**
+ * @apiDefine master Master access only
+ * You must pass `access_token` parameter or a Bearer Token authorization header
+ * to access this endpoint.
+ */
+/**
+ * @apiDefine admin Admin access only
+ * You must pass `access_token` parameter or a Bearer Token authorization header
+ * to access this endpoint.
+ */
+/**
+ * @apiDefine user User access only
+ * You must pass `access_token` parameter or a Bearer Token authorization header
+ * to access this endpoint.
+ */
+/**
+ * @apiDefine listParams
+ * @apiParam {String} [q] Query to search.
+ * @apiParam {Number{1..30}} [page=1] Page number.
+ * @apiParam {Number{1..100}} [limit=30] Amount of returned items.
+ * @apiParam {String[]} [sort=-createdAt] Order of returned items.
+ * @apiParam {String[]} [fields] Fields to be returned.
+ */
+
+router.get('/render', function (req, res) {
+    var url = req.query.url;
+    if (url) {
+        var converted = convert(url);
+
+        converted.on("error", function (e) {
+            res.status(500).send(e.toString());
+
+        });
+        converted.pipe(res);
+    }
+    else
+        res.sendStatus(400);
+});
+
+export default router
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import http from 'http'
+import { env, mongo, port, ip } from './config'
+import express from './services/express'
+import api from './api'
+
+const app = express(api)
+const server = http.createServer(app)
+
+
+setImmediate(() => {
+  server.listen(port, ip, () => {
+    console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
+  })
+})
+
+
+export default app
